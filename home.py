aa90439de5408148b118f93ae6d51b31fcf2b3e7
mati-ldn <48890846+mati-ldn@users.noreply.github.com>
2023-08-07 17:00:13 +0100

    Update home.py
    handle runtime exception

--- home.py
+++ home.py
@@ -1,14 +1,19 @@
 import sys
 import streamlit as st
+from streamlit import runtime
 from streamlit.web import cli as stcli
 
 from cryptorv.data.basis import BasisTbl, BasisSummaryTblAbs, BasisSummaryTblPct
 
 
-df = BasisSummaryTblPct().load()
-st.table(df)
+def main():
+    df = BasisSummaryTblPct().load()
+    st.table(df)
 
 
 if __name__ == '__main__':
-    sys.argv = ['streamlit', 'run', 'home.py']
-    sys.exit(stcli.main())
+    if runtime.exits():
+        main()
+    else:
+        sys.argv = ['streamlit', 'run', 'home.py']
+        sys.exit(stcli.main())
